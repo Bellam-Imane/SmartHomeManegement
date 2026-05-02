87183a155f3b671eed3c25e8006ffacaebcc331e
Bellam Imane <imanebellam4@gmail.com>
2026-05-02 15:13:38 +0200

    fix(layout): enlever le scroll du sidebar et déplacer le scroll vers l’outlet, correction de l’espace entre les deux

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -8,12 +8,12 @@ const MainLayout = () => {
     <div className="flex min-h-screen bg-gray-50 w-full overflow-x-hidden">
       
       
-      <aside className="w-24 fixed left-0 top-0 h-screen flex items-center justify-center z-50 p-2">
+      <aside className="w-24 min-h-screen flex items-center justify-center p-2">
         <Sidebar />
       </aside>
 
       
-      <main className="flex-1 ml-24 p-4 flex flex-col min-h-screen w-full">
+      <main className="flex-1 p-4 flex flex-col min-h-screen w-full">
         
         
         <div className="flex-1 w-full h-full">
