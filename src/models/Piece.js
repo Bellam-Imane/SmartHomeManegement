a3c032a702c1b052f770ee85d0e471eca19a7938
Bellam Imane <imanebellam4@gmail.com>
2026-05-12 19:15:35 +0200

    feat: ajout du modèle Mongoose pour les pièces

---
+++ src/models/Piece.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const pieceSchema = new mongoose.Schema({
+    nomPiece:{
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    type:{
+        type: String,
+        required: true,
+        enum: ['Salon','Chambre à coucher','Cuisine','Toilette','Bureau','Autre'],
+        default:'Salon'
+    },
+
+    superficie:{
+        type: String,
+        required: true
+    },
+
+    etage:{
+        type: Number,
+        default: 0  // 0 pour le rez-de-dhaussée
+    },
+
+    maison:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Maison',
+        required: true
+    },
+
+    appareils: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Appareil'
+
+    }]
+
+},{timestamps: true});
+
+module.exports = mongoose.model('Piece', pieceSchema) ;
